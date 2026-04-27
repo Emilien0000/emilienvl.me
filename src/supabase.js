fde414594666ada4cdcfb4c15a33aadb66fa31a7
Emilien Vitry-Lhotte <emilien.vitry-lhotte@etu.unilasalle.fr>
2026-04-27 16:55:06 +0200

    Add files via upload

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'netijudyxisipmcszjty/sql/c84fe803-3d34-4a16-81b6-810eb5769804.supabase.co';
+const supabaseUrl = 'https://netijudyxisipmcszjty.supabase.co';
 const supabaseKey = 'sb_publishable_sIUI2NF0aNTbBVRVTk-B6g_EVKgp00j';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
